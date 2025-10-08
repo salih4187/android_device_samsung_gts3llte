af9a94b4b3df022078fe1be395ad06af193fb02c
Salih Çağlar <salihcaglar4187@gmail.com>
2025-10-08 14:13:16 +0300

    gts3llte: Modify manifest for ril blobs

--- device.mk
+++ device.mk
@@ -42,8 +42,9 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.5 \
-    android.hardware.radio.config@1.2 \
+    android.hardware.radio@1.0 \
+    android.hardware.radio@1.1 \
+    android.hardware.radio@1.2 \
     android.hardware.radio.deprecated@1.0 \
     android.hardware.secure_element@1.2 \
     librmnetctl \
