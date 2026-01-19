81ea7278ae795e6cef1b7c36c2ef643a73dec5f2
Salih Çağlar <salihcaglar4187@gmail.com>
2026-01-19 17:34:12 +0300

    Revert "gts3llte: Modify manifest for ril blobs"
    This reverts commit af9a94b4b3df022078fe1be395ad06af193fb02c.

--- device.mk
+++ device.mk
@@ -42,9 +42,8 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.0 \
-    android.hardware.radio@1.1 \
-    android.hardware.radio@1.2 \
+    android.hardware.radio@1.5 \
+    android.hardware.radio.config@1.2 \
     android.hardware.radio.deprecated@1.0 \
     android.hardware.secure_element@1.2 \
     librmnetctl \
